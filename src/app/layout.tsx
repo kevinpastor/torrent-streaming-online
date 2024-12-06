b249e98b160d95c76a81f856b70ccf4f01984ac1
Kevin Pastor <kevinpastor.mtl@gmail.com>
2024-12-05 22:35:56 -0500

    feat: update styling with shadcn

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,18 @@
+import { GeistSans } from "geist/font/sans";
+import { GeistMono } from "geist/font/mono";
 import { PropsWithChildren, ReactNode } from "react";
 
 import "./global.css"
 
 const RootLayout = ({ children }: Readonly<PropsWithChildren>): ReactNode => (
-    <html lang="en">
+    <html lang="en" className={`dark ${GeistSans.variable} ${GeistMono.variable}`}>
         <body>
-            {children}
+            <div className="max-w-5xl mx-auto p-4 flex flex-col gap-4">
+                <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+                    Torrent Streaming Online
+                </h1>
+                {children}
+            </div>
         </body>
     </html>
 )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 "use client"
 
-import { MediaPlayer, MediaPlayerInstance, MediaProvider, Poster } from '@vidstack/react';
+import { MediaPlayer, MediaPlayerInstance, MediaProvider } from '@vidstack/react';
 import { defaultLayoutIcons, DefaultVideoLayout } from '@vidstack/react/player/layouts/default';
 import { ReactNode, useEffect, useRef, useState } from "react"
 import WebTorrent from "webtorrent"
 
 import '@vidstack/react/player/styles/default/theme.css';
 import '@vidstack/react/player/styles/default/layouts/video.css';
+import { Button } from '~/components/ui/button';
+import { Input } from '~/components/ui/input';
+import { Loader2 } from 'lucide-react';
 
 const Page = (): ReactNode => {
     const [isInitializing, setIsInitializing] = useState(true)
@@ -75,12 +78,12 @@ const Page = (): ReactNode => {
     }
 
     return (
-        <div className="p-4 flex flex-col gap-4">
+        <>
             <form
                 action={submit}
-                className="flex justify-between items-end gap-"
+                className="flex justify-between items-end gap-4"
             >
-                <input
+                <Input
                     type="text"
                     id="magnet"
                     autoComplete="off"
@@ -88,35 +91,37 @@ const Page = (): ReactNode => {
                     placeholder="Magnet"
                     defaultValue="magnet:?xt=urn:btih:08ada5a7a6183aae1e09d831df6748d566095a10&dn=Sintel&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel.torrent"
                 />
-                <button
+                <Button
                     type="submit"
                     disabled={isInitializing}
                 >
+                    <Loader2 className="animate-spin" />
                     Load
-                </button>
+                </Button>
             </form>
-            <MediaPlayer
-                ref={playerRef}
-                title="Sprite Fight"
-                src={streamUrl}
-                aspectRatio="16/9"
-                viewType="video"
-                load="custom"
-                posterLoad="eager"
-                playsInline
-            >
-                <MediaProvider>
-                    <Poster
-                        src="https://files.vidstack.io/sprite-fight/poster.webp"
-                        alt="Girl walks into campfire with gnomes surrounding her friend ready for their next meal!"
+            {streamUrl ?
+                <MediaPlayer
+                    ref={playerRef}
+                    src={streamUrl}
+                    aspectRatio="16/9"
+                    viewType="video"
+                    load="custom"
+                    posterLoad="eager"
+                    playsInline
+                >
+                    <MediaProvider />
+                    <DefaultVideoLayout
+                        colorScheme="dark"
+                        icons={defaultLayoutIcons}
                     />
-                </MediaProvider>
-                <DefaultVideoLayout
-                    colorScheme="dark"
-                    icons={defaultLayoutIcons}
-                />
-            </MediaPlayer>
-        </div>
+                </MediaPlayer>
+                : <div className="w-full aspect-video rounded-xl border bg-card text-card-foreground shadow flex items-center justify-center">
+                    <span className="leading-7">
+                        Paste a magnet above
+                    </span>
+                </div>
+            }
+        </>
     );
 }
 
